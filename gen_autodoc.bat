45f10f6673142d5326822fc3e18177f875493996
bubbob42 <bubbob42@users.noreply.github.com>
2018-05-02 23:31:03 +0200

    Batch script for generation of autodocs & includes

---
+++ gen_autodoc.bat
@@ -0,0 +1,1 @@
+adoc --table-of-contents ACA1233n.library.asm > developer/autodoc/ACA1233n.doc
---
+++ gen_includes.bat
@@ -0,0 +1,4 @@
+fd2pragma Projects:ACA1233n.library/developer/include/fd/ACA1233n_lib.fd CLIB Projects:ACA1233n.library/developer/include/c/clib/ACA1233n_protos.h special 40 to Projects:ACA1233n.library/developer/include/c/inline/
+fd2pragma Projects:ACA1233n.library/developer/include/fd/ACA1233n_lib.fd special 35 to Projects:ACA1233n.library/developer/include/c/proto/
+fd2pragma Projects:ACA1233n.library/developer/include/fd/ACA1233n_lib.fd to Projects:ACA1233n.library/developer/include/c/pragma/
+fd2pragma Projects:ACA1233n.library/developer/include/fd/ACA1233n_lib.fd special 20 to Projects:ACA1233n.library/developer/include/asm/lvo/
